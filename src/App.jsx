7d658e34cf998d831cce856b0be5e86f501f60e1
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-09 05:23:49 +0200

    Adeed footer and navigation link

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,32 @@ import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import Layout from "./Components/Layout"
+import HostLayout from "./Components/HostLayout"
+
+
 import "../server"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout/>}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-        <Route path="/host" element={<Dashboard />} />
-        <Route path="/host/income" element={<Income />} />
-        <Route path="/host/reviews" element={<Reviews />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
-      
       </Routes>
     </BrowserRouter>
   )
 }
 
-/*ReactDOM
+ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />);*/
+  .render(<App />);
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -5,9 +5,10 @@ import Header from "./Header"
 export default function Layout() {
     return (
         <>
+
         <Header/>
-        <h1>This is the Layout Route</h1>
         <Outlet/>
+        
         </>
     )
 }
