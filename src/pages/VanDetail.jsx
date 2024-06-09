f205b8cae50515ebb6c36ae4e1b872ff597796e9
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-09 16:36:35 +0200

    Updated my fetch request using API

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,18 +1,31 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom";
+//import { getVans } from "../api";
 
-export default function VanDetail() {
+export default function VanDetail () {
     const params = useParams()
-    const [van, setVan] = React.useState(null)
+    const location = useLocation()
+    console.log(location)
 
+    const [van, setVan] = React.useState(null)
+    
     React.useEffect(() => {
         fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
             .then(data => setVan(data.vans))
     }, [params.id])
 
+    const search = location.state?.search || ""
+    const type = location.state?.type || "all"
+
     return (
         <div className="van-detail-container">
+        <Link 
+            to={`..${search}`} 
+            relative="path"
+            className="back-button"
+            >&larr; <span>back to {type} vans</span></Link> 
+            
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
