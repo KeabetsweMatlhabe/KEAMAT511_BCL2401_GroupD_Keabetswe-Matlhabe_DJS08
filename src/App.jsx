c93c4c8d9579a33dd2e631c7458b5009a1bad194
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-10 14:32:53 +0200

    Installed react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,54 @@
-//import React from 'react';
+import React from "react";
+//import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
-import NotFound from "./pages/NotFound";
-import Dashboard from './pages/Host/Dashboard';
-import Income from './pages/Host/Income';
-import Reviews from './pages/Host/Reviews';
-import Layout from "./components/Layout";
-import HostLayout from "./components/HostLayout";
+import Vans from "./pages/Van/Vans";
+import VanDetail from "./pages/Van/VanDetail";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
 import HostVans from "./pages/Host/HostVans";
 import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
-import "./server"
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
+import AuthRequired from "./components/AuthRequired";
 
-
+import "./server";
 
 export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}/>
-            <Route path="about" element={<About />}/>
-            
-            <Route path="vans" element={<Vans />}/>
-            <Route path="vans/:id" element={<VanDetail />}/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="login" element={<Login />} />
 
+          <Route element={<AuthRequired />}>
             <Route path="host" element={<HostLayout />}>
-              <Route index element={<Dashboard />}/>
-              <Route path="income" element={<Income />}/>
-              <Route path="reviews" element={<Reviews />}/>
-              <Route path="vans" element={<HostVans/>}/>
-              <Route path="vans/:id" element={<HostVanDetail/>}>
-                <Route index element={<HostVanInfo/>}/>
-                <Route path="pricing" element={<HostVanPricing/>}/>
-                <Route path="Photos" element={<HostVanPhotos />}/>
-              </Route>            
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
-            <Route path="*" element={<NotFound />}/>
-          </Route>  
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
